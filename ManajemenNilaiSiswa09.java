78a7454106313859dd88a64fc61075cc5959e1d7
ayinanda <ayiniswati22@gmail.com>
2024-10-25 10:57:24 +0700

    Nilai siswa

--- ManajemenNilaiSiswa09.java
+++ ManajemenNilaiSiswa09.java
@@ -5,54 +5,50 @@ public class ManajemenNilaiSiswa09 {
     public static void main(String[] args) {
         Scanner sc09 = new Scanner(System.in);
 
-    String namaSiswa; 
-    int jumlahSiswa = 0, nilaiAtasRata = 0, nilaiBawahRata = 0;
-    double nilai = 0, totalNilai = 0 , rataRata = 0 , rataRataAkhir = 0;
-    boolean mayoritasNilaiAtasMean = false;
-
-    while (true) {
-        System.out.print(" Masukkan nama siswa :" + namaSiswa);
-        namaSiswa = sc09.nextLine();
-        sc09.nextLine();
-
-        if (namaSiswa.equalsIgnoreCase("selesai")) {
-            break;
-        } 
-        System.out.println(" Masukkan nilai " + namaSiswa + " : ");
-        nilai = sc09.nextInt();
-        sc09.nextLine();
+        int totalSiswa = 0, atasMean = 0, bawahMean = 0;
+        double nilai = 0, totalNilai = 0, meanAkhir;
+        String nama;
+        boolean mayoritasNilai;
         
-        if (nilai>75) {
-            System.out.println("nilai di atas rata rata ");
-            nilai = sc09.nextInt();
+        while (true) {
+            System.out.println("Silahkan Ketik 'Selesai' untuk berhenti");
+            System.out.print("Masukkan nama siswa: ");
+            nama = sc09.nextLine();
+        
+            if (nama.equalsIgnoreCase("selesai")) {
+                break;
+            }
+        
+            System.out.print("Masukkan nilai siswa: ");
+            nilai = sc09.nextDouble();
+            sc09.nextLine();
+        
+            totalSiswa ++;
+            totalNilai += nilai;
+           
+                if (nilai >= 75) {
+                    atasMean++;
+                } else {
+                    bawahMean++;
+                }
+            } 
+        
+            if (totalSiswa > 0) {
+                meanAkhir = totalNilai / totalSiswa;
+        
+            mayoritasNilai = atasMean > (totalSiswa / 2.0);
+        
+        System.out.println("-------------------------------------");
+        System.out.println("-------Laporan Penilaian Kelas-------");
+        System.out.println("-------------------------------------");
+        System.out.println("Rata-rata Nilai Kelas: " + meanAkhir);
+        System.out.println("Jumlah Siswa Memiliki Nilai di Atas Rata-rata: " + atasMean);
+        System.out.println("Jumlah Siswa Memiliki Nilai di Bawah Rata-rata: " + bawahMean);
+        System.out.println("Apakah Mayoritas Nilai Siswa di Atas Rata-rata? " + (mayoritasNilai ? "Ya" : "Tidak"));
+        
         } else {
-            System.out.println("nilai di bawah rata rata ");
-            nilai = sc09.nextInt();
+            System.out.println("Tidak ada nilai yang dimasukkan.");
         }
-
-        jumlahSiswa++ ;
-        totalNilai += nilai ;
-
-        rataRata = totalNilai/jumlahSiswa;
-
-        if (nilai > rataRata) {
-            nilaiAtasRata++;
-        } else {
-            nilaiBawahRata++;
-        } 
-        mayoritasNilaiAtasMean = (nilaiAtasRata > jumlahSiswa) ;
-        mayoritasNilaiAtasMean= sc09.nextBoolean();
-    } 
-    System.out.println("---------------------HASIL AKHIR NILAI SISWA-----------------------");
-    System.out.println(" rata-rata nilai siswa dalam satu kelas: " + rataRata);
-
-    if (mayoritasNilaiAtasMean) {
-        System.out.println("Mayoritas siswa memiliki nilai di atas rata rata");
-        mayoritasNilaiAtasMean = sc09.nextBoolean();
-        sc09.nextLine();
-    } else {
-        System.out.println("Mayoritas siswa memiliki nilai di bawah rata rata");
-    }
     }
 }
 
